7a97859799b6292ad34fc7447b791fd85d20590e
毛贤德 <394097441@qq.com>
2018-12-18 10:21:35 +0800

    严格模式

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,8 +1,10 @@
 import './vuex'
 import Vuex from 'vuex'
 import modules from './modules'
-
+let strict = process.env.NODE_ENV !== 'production'
+console.log('strict', strict)
 export default new Vuex.Store({
+  strict,
   modules: {
     ...modules
   }
