b5b0a12f7f9fe21b2684f607130e25752a7904cc
ylinn <2362856240@qq.com>
2018-12-26 09:27:44 +0800

    入口文件样式修改，引入组件

--- src/app/index.vue
+++ src/app/index.vue
@@ -18,6 +18,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
 }
 </style>
