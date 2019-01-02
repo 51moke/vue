5767f51e5a63f7503c6c837ef4d94887e6a31798
ylinn <2362856240@qq.com>
2019-01-03 00:04:20 +0800

    menu展开关闭的修复

--- src/stores/modules/layout/index.js
+++ src/stores/modules/layout/index.js
@@ -1,9 +1,8 @@
 // import { api } from 'apibus'
 export const namespaced = true
 export const state = {
-  isCollapse: sessionStorage.isCollapse
+  isCollapse: !!sessionStorage.isCollapse
 }
-
 export const getters = {
 
 }
@@ -11,11 +10,9 @@ export const getters = {
 export const mutations = {
   getIsCollapse (state, data) {
     state.isCollapse = !state.isCollapse
-    sessionStorage.isCollapse = state.isCollapse
+    sessionStorage.isCollapse = state.isCollapse ? 1 : ''
   }
-
 }
-
 export const actions = {
   changeIsCollapse (context) {
     context.commit('getIsCollapse')
